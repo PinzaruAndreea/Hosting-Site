032910c2d9b0ebe117fcc974eb1c6e8b622005c6
PinzaruAndreea <66926634+PinzaruAndreea@users.noreply.github.com>
2021-01-29 00:25:32 +0200

    Update shared.js

--- Hosting site/shared.js
+++ Hosting site/shared.js
@@ -4,17 +4,14 @@ var modalNoButton = document.querySelector(".modal__action--negative");
 var selectPlanButtons = document.querySelectorAll(".plan button");
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
-var ctaButton = document.querySelector("main-nav__item--cta");
+var ctaButton = document.querySelector("main-nav__item--cta")
 
 // console.dir(backdrop.style['background-image']);
 
 // console.dir(backdrop);
 for (var i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", function() {
-    // modal.style.display = "block";
-    // backdrop.style.display = "block";
-    // modal.className = 'open'; // This will actually overwrite the complete class list
-    modal.classList.add("open");
+    modal.classList .add("open");
     backdrop.style.display = "block";
     setTimeout(function() {
       backdrop.classList.add("open");
@@ -29,7 +26,7 @@ backdrop.addEventListener("click", function() {
 });
 
 if (modalNoButton) {
-  modalNoButton.addEventListener("click", closeModal());
+  modalNoButton.addEventListener("click", closeModal);
 }
 
 function closeModal() {
@@ -53,13 +50,3 @@ toggleButton.addEventListener("click", function() {
     backdrop.classList.add("open");
   }, 10);
 });
-
-ctaButton.addEventListener("animationstart", function{
-	console.log("Animation started", event);
-})
-ctaButton.addEventListener("animationend", function {
-	console.log("Animation ended", event);
-})
-ctaButton.addEventListener("animationiteration", function {
-	console.log("Animation iteration", event);
-})
